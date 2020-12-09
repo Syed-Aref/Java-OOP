e7fface0660a458d361ac4601caacfa4e24356e9
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2020-12-09 21:30:07 +0600

    Update SimpleClassExample.java

--- SimpleClassExample.java
+++ SimpleClassExample.java
@@ -6,25 +6,27 @@ class Info
 	public int id , sec;
 	public Info(String name,int id,int sec)
 	{
-		this.name = name;
-		this.id = id;
+		this.name = name; // this refrers to variables that belong to class Info
+		this.id = id;S
 		this.sec = sec;
 	}
-	public String toString()
+	public String toString() 
 	{
 		return "Name : "+this.name+" , "+"Id : "+this.id+" , "+"Section : "+this.sec;
 	}
+	// Normally , S.o.pln(info_var) prints memory location of info_var
+	// Now,it will print the string that returned in line 15
 }
 
 public class SimpleClassExample {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Info i1 = new Info("Hasan",20,02);
+		Info i1 = new Info("Hasan",20,02); // Creating new instance
 		Info i2 = i1;
 		Info i3 = i2;
 		
-		i2 = null;
+		i2 = null; // Nullyfying i2 , however i1 and i3 are not affected
 		
 		System.out.println(i1);
 		System.out.println(i2);
