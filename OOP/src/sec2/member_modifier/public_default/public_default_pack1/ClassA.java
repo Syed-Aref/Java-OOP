d605f99ac8988c057db4c2382ce96a33a40c5447
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-01-30 11:31:37 +0600

    Create ClassA.java

---
+++ OOP/src/sec2/member_modifier/public_default/public_default_pack1/ClassA.java
@@ -0,0 +1,10 @@
+package sec2.member_modifier.public_default.public_default_pack1;
+
+public class ClassA {
+    void m1() {
+        System.out.println("This method is ClassA.m1()");
+    }
+    public void m2() {
+        System.out.println("This method is ClassA.m2()");
+    }
+}
